509f5df55a9f2e882d187b8a98e40ed3c4f75396
Hyvärinen Santtu <bgg317@myy.haaga-helia.fi>
2019-09-09 09:53:09 +0300

    Added edit functionality

--- src/main/java/com/example/bookstore/web/BookController.java
+++ src/main/java/com/example/bookstore/web/BookController.java
@@ -32,6 +32,7 @@ public class BookController {
 	}
 	@RequestMapping(value="/add")
 	public String addBook(Model model) {
+		model.addAttribute("title","Add book");
 		model.addAttribute("book", new Book());
 		model.addAttribute("categories", categoryRepository.findAll());
 		return	"addbook";
@@ -47,6 +48,14 @@ public class BookController {
 	public String deleteBook(@PathVariable("id") Long bookId,Model model) {
 		repository.deleteById(bookId);
 		return "redirect:/booklist";
+	}
+	
+	@RequestMapping(value="/edit/{id}")
+	public String editBook(@PathVariable("id") Long bookId, Model model) {
+		model.addAttribute("title","Edit book");
+		model.addAttribute("book", repository.findById(bookId));
+		model.addAttribute("categories", categoryRepository.findAll());
+		return "addbook";
 	}
 }
 
